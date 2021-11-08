82c15b6462d9ab9a67bdd7321dabb4f3031e2d8f
Philémon MOREL <p.morel@rubika-edu.com>
2021-11-08 02:42:07 +0100

    Mise en place du Player Controller
    . Création des scripts nécessaires au contrôle du personnage
    . Mise en place des animations pour le déplacement et l'attaque
    . Réalisation des différents éléments de l'inspector
    
    A FAIRE :
    -Mettre les values correctes sur les scripts PlayerBehaviour, Cursor Behaviour et BulletPoolBehaviour
    -Mettre les animations correspondantes au joueur

---
+++ TheRealSka8erBoi/Assets/Code/Scripts/Player/Core/CursorBehaviour.cs
@@ -0,0 +1,127 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorBehaviour : MonoBehaviour
+{
+    [SerializeField] Vector2 leftJoy;
+    public GameObject EnemyDetector;
+    [SerializeField]private float angle;
+    private Quaternion target;
+    private Vector2 closestDirection;
+    private List<Vector2> closeEnemies;
+    private Vector2 closestEnemy;
+    private float minAngle;
+    [SerializeField] private float smoothSpeed;
+    [SerializeField] private float autoAimField;
+    
+    
+
+    private void OnEnable()
+    {
+        leftJoy = GetComponentInParent<PlayerBehaviour>().latestDirection;
+        transform.rotation = Quaternion.Euler(0f, 0f, Mathf.Atan2(leftJoy.y, leftJoy.x) * Mathf.Rad2Deg + 90);
+        closestEnemy = new Vector2(200, 200);
+        if (GetClosestEnemies().Count != 0)
+        {
+            foreach (var direc in GetClosestEnemies())
+            {
+                if (Mathf.Abs(direc.x) + Mathf.Abs(direc.y) < Mathf.Abs(closestEnemy.x) + Mathf.Abs(closestEnemy.y))
+                {
+                    closestEnemy = direc;
+                }
+            }
+            transform.rotation = Quaternion.Euler(0f, 0f, Mathf.Atan2(closestEnemy.y, closestEnemy.x) * Mathf.Rad2Deg + 90);
+        }
+        
+    }
+    
+    void FixedUpdate()
+     {
+         if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0.3f || Mathf.Abs(Input.GetAxisRaw("Vertical")) > 0.3f)
+         {
+             leftJoy.x = Input.GetAxisRaw("Horizontal");
+             leftJoy.y = Input.GetAxisRaw("Vertical");
+         }
+         target = AimDirection().Item1; 
+         transform.rotation = Quaternion.Lerp(transform.rotation, target, smoothSpeed);
+     }
+
+    
+
+    private Vector2 GetClosestEnemy()
+     {
+         Transform closestEnemy = null;
+         float minDist = Mathf.Infinity;
+         foreach (GameObject enemy in EnemyDetector.GetComponent<EnemyDetector>().closeEnemies)
+         {
+             float dist = Vector2.Distance(enemy.transform.position, transform.position);
+             if (minDist > dist )
+             {
+                 minDist = dist;
+                 closestEnemy = enemy.transform;
+             }
+         }
+
+         if (closestEnemy != null)
+         {
+             return new Vector2(closestEnemy.position.x - transform.position.x, closestEnemy.position.y - transform.position.y);   
+         }
+         else
+         {
+             return Vector2.zero;
+         }
+
+     }
+     
+     
+     private List<Vector2> GetClosestEnemies()
+     {
+         List<Vector2> direcCloseEnemies = new List<Vector2>();
+         foreach (GameObject enemy in EnemyDetector.GetComponent<EnemyDetector>().closeEnemies)
+         {
+             direcCloseEnemies.Add(new Vector2(enemy.transform.position.x - transform.position.x, enemy.transform.position.y - transform.position.y));
+         }
+
+         return direcCloseEnemies;
+     }
+
+     public Tuple<Quaternion,Vector2> AimDirection()
+     {
+         closeEnemies = GetClosestEnemies();
+
+         if (closeEnemies.Count != 0)
+         {
+             minAngle = 200;
+             foreach (Vector2 dirEnemy in closeEnemies)
+             {
+                 
+                 angle = Vector2.Angle(new Vector2(dirEnemy.x, dirEnemy.y), leftJoy);
+                 
+                 if (minAngle > angle)
+                 {
+                     minAngle = angle;
+                     closestDirection = dirEnemy;
+                 }
+                 
+             }
+             if (minAngle < autoAimField)
+             {
+                 angle = Mathf.Atan2(closestDirection.y, closestDirection.x) * Mathf.Rad2Deg;
+                 return Tuple.Create(Quaternion.Euler(0f,0f,angle),closestDirection);
+                 
+             }
+             else
+             {
+                 angle = Mathf.Atan2(leftJoy.y, leftJoy.x) * Mathf.Rad2Deg;
+                 return Tuple.Create(Quaternion.Euler(0f,0f,angle),leftJoy);
+             }
+         }
+         else
+         {
+             angle = Mathf.Atan2(leftJoy.y, leftJoy.x) * Mathf.Rad2Deg;
+             return Tuple.Create(Quaternion.Euler(0f,0f,angle),leftJoy);
+         }
+     }
+}
---
+++ TheRealSka8erBoi/Assets/Code/Scripts/Player/Shoot Parameters/PoolObjectManager.cs
@@ -0,0 +1,122 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolObjectManager : MonoBehaviour
+{
+    // Création de la Class Bullet qui permet de renseigner les différents types de balles, avec le nom, le prefab ainsi que la taille de la pool
+    [Serializable]
+    public class Bullet
+    {
+        public string name;
+        public GameObject prefab;
+        public int size;
+    }
+    
+    // Création d'une class copie de Bullet mais en tant que prefab afin de plus facilement accéder aux données via les autres scripts, notamment avec le BulletPoolBehavior
+    [Serializable]
+    public class BulletPrefab
+    {
+        public string name;
+        public GameObject prefab;
+        public Transform prefabParent;
+
+        public BulletPrefab(string name, GameObject prefab, Transform prefabParent)
+        {
+            this.name = name;
+            this.prefab = prefab;
+            this.prefabParent = prefabParent; 
+        }
+    }
+    
+    // Mise en instance du script afin de l'accèder via d'autre script
+    public static PoolObjectManager Instance;
+    
+    // Mise en place du Dictionnary, fonctionnement du Pool 
+    public List<Bullet> bullets;
+    public Dictionary<string, Queue<GameObject>> PoolDictionary;
+    [HideInInspector] List<BulletPrefab> prefabList = new List<BulletPrefab>(); 
+    
+    //Mise en place de l'instance
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    //Création de la pool
+    private void Start()
+    {
+        PoolDictionary = new Dictionary<string, Queue<GameObject>>();
+        
+        //En gros, pour chaque balle mis dans la classe, tu créés un parent qui va contenir toutes les balles et tu mets dans la pool tous les objects que tu mets
+        foreach (Bullet bullet in bullets)
+        {
+            GameObject bulletParent = new GameObject();
+            bulletParent.name = bullet.name + "Pool";
+            GameObject.DontDestroyOnLoad(bulletParent);
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            for (int i = 0; i < bullet.size; i++)
+            {
+                GameObject obj = Instantiate(bullet.prefab, bulletParent.transform);
+                obj.GetComponent<BulletPoolBehaviour>().bulletName = bullet.name;
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+
+            PoolDictionary.Add(bullet.name, objectPool);
+            prefabList.Add(new BulletPrefab(bullet.name, bullet.prefab, bulletParent.transform));
+        }
+    }
+
+    //Ceci correspond à la fonction à call lorsque que tu veux réaliser ton tir
+    public GameObject GetBullet(string bulletName, Vector2 position, Quaternion rotation)
+    {
+        if (PoolDictionary.ContainsKey(bulletName))
+        {
+            if (PoolDictionary[bulletName].Count > 0)
+            {
+                GameObject obj = PoolDictionary[bulletName].Dequeue();
+                obj.transform.position = position;
+                obj.transform.rotation = rotation;
+                obj.SetActive(true);
+                return obj;
+            }
+            else
+            {
+                for (int i = 0; i < prefabList.Count; i++)
+                {
+                    if (bulletName == prefabList[i].name)
+                    {
+                        GameObject obj = Instantiate(prefabList[i].prefab, prefabList[i].prefabParent);
+                        obj.GetComponent<BulletPoolBehaviour>().bulletName = bulletName;
+                        PoolDictionary[bulletName].Enqueue(obj);
+
+
+                        PoolDictionary[bulletName].Dequeue();
+                        obj.transform.position = position;
+                        obj.transform.rotation = rotation;
+                        return obj;
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+    
+    //Fonction a call lorsque tu veux "détruire" la bullet
+    public void DestroyBullet(string bulletName, GameObject poolObject)
+    {
+        if (PoolDictionary.ContainsKey(bulletName))
+        {
+            PoolDictionary[bulletName].Enqueue(poolObject);
+            poolObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            poolObject.SetActive(false);
+        }
+    }
+}
