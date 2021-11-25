9dfe2bf343141adce290a21d3148b0ca084b8a39
Philémon MOREL <p.morel@rubika-edu.com>
2021-11-25 19:29:57 +0100

    V1 : Release
    . Création de  la semaine marchand (Avec Marchand plus animation)
    . Changement et tweaks du HUB
    . Remise en forme de la map S1 avec intégration dans la scène approprié (Mise en place des props)
    . Mise en place des scripts de changements de scènes
    . Changement au sein des prefabs afin de corriger les points importants

--- TheRealSka8erBoi/Assets/Code/Scripts/LoadSceneManagerPROTO.cs
+++ TheRealSka8erBoi/Assets/Code/Scripts/LoadSceneManagerPROTO.cs
@@ -7,9 +7,15 @@ public class LoadSceneManagerPROTO : MonoBehaviour
 {
     public List<string> roomOrder;
     public int numberOfRoom;
+    public static LoadSceneManagerPROTO LoadSceneManagerProtoInstance;
     
     public void Awake()
     {
+        if (LoadSceneManagerProtoInstance == null)
+        {
+            LoadSceneManagerPROTO.LoadSceneManagerProtoInstance = this;
+        }
+        
         DontDestroyOnLoad(this.gameObject);
         numberOfRoom = 0;
     }
