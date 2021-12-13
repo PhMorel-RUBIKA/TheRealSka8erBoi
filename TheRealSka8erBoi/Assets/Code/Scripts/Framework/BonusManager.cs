b0c5d1a1f583bf1acc3dbd9b9fecef427e62dda6
Grégoire Marty <g.marty@rubika-edu.com>
2021-12-13 16:22:23 +0100

    Bonus Manager

--- TheRealSka8erBoi/Assets/Code/Scripts/Framework/BonusManager.cs
+++ TheRealSka8erBoi/Assets/Code/Scripts/Framework/BonusManager.cs
@@ -21,7 +21,7 @@ public class BonusManager : MonoBehaviour
     private void Start()
     {
         redStat = 0;
-        greenStat = 0;
+        greenStat = 2;
         blueStat = 0;
         money = 0;
     }
