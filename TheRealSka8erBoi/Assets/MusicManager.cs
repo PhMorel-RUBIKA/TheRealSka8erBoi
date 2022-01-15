ef56df38ba3559646dfea856032efaef3a71d870
Romain Cappelletti <r.cappelletti@rubika-edu.com>
2022-01-15 20:44:05 +0100

    Musiques posées (sauf sur la salle du boss)
    faudra quand même peut-être faire quelques réglages

---
+++ TheRealSka8erBoi/Assets/MusicManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    [SerializeField] private AudioSource aSource;
+    [SerializeField] private AudioClip intro;
+    [SerializeField] private AudioSource core;
+    void Update()
+    {
+
+    }
+}
